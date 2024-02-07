ecf143e2e6786712544cee8aef01a1fbe5d130c4
Jekbe <patrykrusakowicz@gmail.com>
2024-02-07 14:06:56 +0100

    cli end

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ import java.util.Scanner;
 
 public class Main {
     private static final Scanner s = new Scanner(System.in);
-    static Socket socket;
+    private static Socket socket;
     private static String login = "";
 
     public static void main(String[] args) {
